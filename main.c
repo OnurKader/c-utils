5d037765b261671312a837f5ac1459d5a239cfbd
OnurKader <onurkader@protonmail.com>
2020-01-25 13:55:00 +0000

    Added basic stuff to quadtree

--- main.c
+++ main.c
@@ -1,35 +1,39 @@
 #include "doub_link_list.h"
+#include "quadtree.h"
 
 #include <stdio.h>
 
 int main(void)
 {
-	List *list;
-	if(!list_init(&list))
-	{
-		list_push(list, 1L);
-		list_push(list, 74L);
-		list_push(list, -1L);
-		list_push(list, 2309L);
-		/* list_push(list, 405L); */
-		/* list_push(list, 0L); */
-		/* list_push(list, -2309L); */
+	/* List *list; */
+	/* if(!list_init(&list)) */
+	/* { */
+	/* 	list_push(list, 1L); */
+	/* 	list_push(list, 74L); */
+	/* 	list_push(list, -1L); */
+	/* 	list_push(list, 2309L); */
+	/* 	list_push(list, 405L); */
+	/* 	list_push(list, 0L); */
+	/* 	list_push(list, -2309L); */
 
-		/* list_insert(list, 69L, 3UL); */
-		/* list_insert(list, 420L, 6UL); */
-		/* list_insert(list, -69420L, 2UL); */
-		/* list_insert(list, 69420L, 0UL); */
+	/* 	list_insert(list, 69L, 3UL); */
+	/* 	list_insert(list, 420L, 6UL); */
+	/* 	list_insert(list, -69420L, 2UL); */
+	/* 	list_insert(list, 69420L, 0UL); */
 
-		list_print(list);
+	/* 	list_print(list); */
 
-		printf("popping: %ld, %ld, %ld, %ld\n",
-			   list_pop(list),
-			   list_pop(list),
-			   list_pop(list),
-			   list_pop(list));
+	/* 	printf("popping: %ld, %ld, %ld, %ld\n", */
+	/* 		   list_pop(list), */
+	/* 		   list_pop(list), */
+	/* 		   list_pop(list), */
+	/* 		   list_pop(list)); */
 
-		list_destroy(list);
-	}
+	/* 	list_destroy(list); */
+	/* } */
+
+	QuadTree* qt;
+
 	return 0;
 }
 
--- quadtree.h
+++ quadtree.h
@@ -2,8 +2,15 @@
 #define QUADTREE_H
 
 #include <math.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+
+#define center centre
+
+#ifndef BUCKET_SIZE
+#	define BUCKET_SIZE 4U
+#endif
 
 float dist(float a, float b, float c, float d)
 {
@@ -14,6 +21,41 @@ typedef struct Point
 {
 	float x, y;
 } Point;
+
+typedef struct Rect
+{
+	Point origin;
+	Point center;
+	float w, h;
+} Rect;
+
+bool pointInRect(const Point* const p, const Rect* const r)
+{
+	return ((p->x >= r->origin.x) && (p->x < r->origin.x + r->w) && (p->y >= r->origin.y) &&
+			(p->y < r->origin.y + r->h));
+}
+
+Point makePoint(float x, float y) { return (Point){x, y}; }
+
+Rect makeRect(float x, float y, float w, float h)
+{
+	Rect temp;
+	temp.origin.x = x;
+	temp.origin.y = y;
+	temp.w = w;
+	temp.h = h;
+	temp.center.x = x + w / 2.f;
+	temp.center.y = y + h / 2.f;
+	return temp;
+}
+
+typedef struct QuadTree
+{
+	struct QuadTree* north_west;
+	struct QuadTree* north_east;
+	struct QuadTree* south_west;
+	struct QuadTree* south_east;
+} QuadTree;
 
 #endif
 
